b308aec2ff51922e4837c3c0fbc87f3f98717650
Dinz <kostan-82@yandex.ru>
2021-03-14 13:42:11 +0300

    3.3. Разбор первой программы. [#496445]

---
+++ src/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Константин Киреев");
+        System.out.println("25.11.2001");
+    }
+}
