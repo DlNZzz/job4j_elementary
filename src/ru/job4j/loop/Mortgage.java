2478d7c46befd8b5858a7a7ff344c10e6dd1fca2
Dinz <kostan-82@yandex.ru>
2021-03-17 18:35:52 +0300

    5.7. Ипотека [#496484]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        percent /= 100;
+        double val = amount;
+
+        while (val > 0) {
+            val += percent * amount - salary;
+            year++;
+        }
+
+        return year;
+    }
+}
